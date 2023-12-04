c6ff8a775c4032d8f1c3fbf8e72e53c2fad4f8cd
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2023-12-04 09:09:14 -0300

    Add files via upload

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,28 +5,25 @@ import Header from "../header/Header";
 
 const Home: React.FC = () => {
     return (
-            <main className={styles.height}>
-                <Header />
-               <div className={styles.container}>
+        <div className={styles.container}>
 
-                <div className={styles.containerMe}>
-                    <h1 className={styles.title}>
-                        Carlos Carvalho
-                    </h1>
-                    <h2 className={styles.subtitle}>Front-End Developer</h2>
+            <div className={styles.containerMe}>
+                <h1 className={styles.title}>
+                    Carlos Carvalho
+                </h1>
+                <h2 className={styles.subtitle}>Front-End Developer</h2>
 
-                    <p className={styles.paragraph}>
-                        Olá, sou Carlos Carvalho, estudante de Sistemas de Informação no IFPR, atualmente no 6º semestre.
-                    </p>
+                <p className={styles.paragraph}>
+                    Olá, sou Carlos Carvalho, estudante de Sistemas de Informação no IFPR, atualmente no 6º semestre.
+                </p>
 
-                    <Button buttonText={"Conheça Minha História"} />
-                </div>
+                <Button buttonText={"Conheça Minha História"} />
+            </div>
 
-                <picture className={styles.containerImg}>
-                    <img src="https://avatars.githubusercontent.com/u/110244419?v=4" alt="Avatar" />
-                </picture>
-               </div>
-            </main>
+            <picture className={styles.containerImg}>
+                <img src="https://avatars.githubusercontent.com/u/110244419?v=4" alt="Avatar" />
+            </picture>
+        </div>
     )
 }
 
